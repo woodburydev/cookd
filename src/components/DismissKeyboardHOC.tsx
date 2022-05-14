829bc5d1dcdaf6802b11d1521ed72d4210287327
woodburydev <54697770+woodburydev@users.noreply.github.com>
2022-05-14 16:38:16 -0700

    Update DismissKeyboardHOC.tsx

--- src/components/DismissKeyboardHOC.tsx
+++ src/components/DismissKeyboardHOC.tsx
@@ -3,6 +3,7 @@ import {ScrollView} from 'react-native-gesture-handler';
 
 const DismissKeyboardHOC = () => {
   return ({children, ...props}: any) => {
+
     return (
       <ScrollView
         keyboardShouldPersistTaps="handled"
