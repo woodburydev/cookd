0f057cff748b6102e6941dfd00bb4acac56e65fc
woodburydev <54697770+woodburydev@users.noreply.github.com>
2022-06-08 10:37:52 -0700

    Update Server URL to Heroku.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 let prod = true;
-const prodUrl = 'http://137.184.246.148';
+const prodUrl = 'https://cookd-server.herokuapp.com';
 let localUrl = 'http://localhost:3000';
 export const endpoint = prod || !__DEV__ ? prodUrl : localUrl;
