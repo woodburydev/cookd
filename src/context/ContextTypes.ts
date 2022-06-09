0dd4bb90d4732ee0d731925c282a47b19146bad4
woodburydev <54697770+woodburydev@users.noreply.github.com>
2022-06-09 14:49:41 -0700

    Update ContextTypes.ts (#25)

--- src/context/ContextTypes.ts
+++ src/context/ContextTypes.ts
@@ -1,7 +1,6 @@
 import {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import {Dispatch, SetStateAction} from 'react';
 
-
 export interface UserContextType {
   user: User;
   setOverrideGet: Dispatch<SetStateAction<boolean>>;
